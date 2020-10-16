bffc84617dbabe1737a4c0c52f39df459d6dabe1
ahmtysn <ahmtysn14@gmail.com>
2020-10-16 13:01:57 +0200

    heroku

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const enableCORS = require('./enableCORS');
 const todosRouter = require('./todosRouter.js');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(enableCORS);
 
 app.use('/todos', todosRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 const PORT = process.env.PORT || 5000;
 
 const server = () => {

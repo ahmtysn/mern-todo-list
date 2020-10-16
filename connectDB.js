ca7166fb97ba7f18515948072672d661357593ed
ahmtysn <ahmtysn14@gmail.com>
2020-10-16 04:13:56 +0200

    style modified

--- connectDB.js
+++ connectDB.js
@@ -5,15 +5,16 @@ const DB_PW = process.env.DB_PW;
 const DB_NAME = process.env.DB_NAME;
 const DB_URL = `mongodb+srv://${DB_USER}:${DB_PW}@cluster0.gb9s6.mongodb.net/${DB_NAME}`;
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
 const connectDB = server => {
   mongoose
-    .connect(DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
-
+    .connect(DB_URL, options)
     .then(() => server())
     .catch(err => console.log('err happened in db connection!', err));
 };

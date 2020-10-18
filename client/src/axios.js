e5d134afd95d39b53daf458aa4bfbf8f5df83b7d
ahmtysn <ahmtysn14@gmail.com>
2020-10-19 01:26:12 +0200

    heroku and github with same url

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,5 +4,5 @@ export default axios.create({
   baseURL: 'https://mernstacktodolist.herokuapp.com/todos',
 });
 
-// herokuURL = https://mernstacktodolist.herokuapp.com/
+// herokuURL = https://mernstacktodolist.herokuapp.com/todos
 // githubURL = http://localhost:5000/todos

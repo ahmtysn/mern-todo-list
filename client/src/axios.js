53706b6c3718cc5f366325ab719d80a4435c15a5
ahmtysn <ahmtysn14@gmail.com>
2020-10-19 01:16:59 +0200

    heroku

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://mernstacktodolist.herokuapp.com/',
+  baseURL: 'https://mernstacktodolist.herokuapp.com/todos',
 });
 
 // herokuURL = https://mernstacktodolist.herokuapp.com/

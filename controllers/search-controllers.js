f0011b6e6164308762608b49a329a1a76a76842c
ahmtysn <ahmtysn14@gmail.com>
2020-10-23 02:24:31 +0200

    search controller add sort

--- controllers/search-controllers.js
+++ controllers/search-controllers.js
@@ -3,7 +3,9 @@ const Todo = require('../models/Todo');
 const searchTodos = async (req, res, next) => {
   let todos;
   try {
-    todos = await Todo.find({ text: { $regex: req.query.q, $options: 'i' } });
+    todos = await Todo.find({
+      text: { $regex: req.query.q, $options: 'i' },
+    }).sort('-isImportant');
   } catch (err) {
     const error = new Error('Fetching todos failed, please try again later.');
     error.status = 500;
